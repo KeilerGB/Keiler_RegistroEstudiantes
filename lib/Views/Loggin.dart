4060b525fcea442d1e55d019d33cdc6670ff6e57
KeilerGB <keiler.cortes@grupobabel.com>
2022-08-03 13:51:45 -0600

    Creación ventana Loggin y rutas de navegacion

---
+++ lib/Views/Loggin.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class Loggin extends StatelessWidget {
+  const Loggin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Loggin"),
+        centerTitle: true,
+      ),
+      body: const Text("Holaaaa"),
+    );
+  }
+}
